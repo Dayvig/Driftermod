55dc27bda5dfdfa1027bdb51ded97feb1384ebe9
Dayvig <49045434+Dayvig@users.noreply.github.com>
2019-08-08 16:44:55 -0400

    Adding new cards!
    Over draw mechanic, drawing and discarding are all themes, just adding cards as I think up them

---
+++ DrifterMod/src/main/java/DrifterMod/powers/TempMaxHandSizeInc.java
@@ -0,0 +1,97 @@
+package DrifterMod.powers;
+
+import DrifterMod.DrifterMod;
+import DrifterMod.util.TextureLoader;
+import basemod.BaseMod;
+import basemod.devcommands.draw.Draw;
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.unique.RetainCardsAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import static DrifterMod.DrifterMod.makePowerPath;
+
+//Gain 1 dex for the turn for each card played.
+
+public class TempMaxHandSizeInc extends AbstractPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = DrifterMod.makeID("TempMaxHandSizeInc");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public TempMaxHandSizeInc(final AbstractCreature owner, final AbstractCreature source, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+        this.source = source;
+
+        type = PowerType.BUFF;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+        this.loadRegion("flex");
+
+        updateDescription();
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+
+    }
+
+    @Override
+    public void onAfterUseCard(AbstractCard c, UseCardAction a){
+        int k = AbstractDungeon.player.hand.size();
+        int j = this.amount;
+        while (k < BaseMod.MAX_HAND_SIZE){
+            AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(this.owner, this.owner, this, 1));
+            if (j > 0) {
+                AbstractDungeon.actionManager.addToBottom(new DrawCardAction(this.owner, 1));
+            }
+            j--;
+            k++;
+            if (j <= 0){
+                AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+                break;
+            }
+        }
+    }
+
+    // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
+    @Override
+    public void updateDescription() {
+        if (amount == 1) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        } else if (amount > 1) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new TempRetainPower(owner, source, amount);
+    }
+}
